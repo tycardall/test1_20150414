cb8656a26d857410b31e379ecf1a1be3288c3de5
Ty Cardall <tycardall@yahoo.com>
2015-04-21 10:48:05 -0700

    20150421

---
+++ ex_3_2.rb
@@ -0,0 +1,18 @@
+#ex_3_2 age.rb
+
+puts "your age?"
+age = gets.chomp.to_i
+age10 = age + 10
+age20 = age + 20
+age30 = age + 30
+age40 = age + 40
+
+puts "In 10 years you will be:"
+puts age10
+puts "In 20 years you will be:"
+puts age20
+puts "In 30 years you will be:"
+puts age30
+puts "In 40 years you will be:"
+puts age40
+
