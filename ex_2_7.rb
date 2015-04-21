f9a68e1a8efd6b840589658c327968812d3d6130
Ty Cardall <tycardall@yahoo.com>
2015-04-21 10:29:25 -0700

    20150421

---
+++ ex_2_7.rb
@@ -0,0 +1,7 @@
+#_2_7
+
+  SyntaxError: (irb):2: syntax error, unexpected ')', expecting '}'
+    from /usr/local/rvm/rubies/ruby-2.0.0-rc2/bin/irb:16:in `<main>'
+    
+    mis-defined variable or hash
+    
---
+++ ex_3_1.rb
@@ -0,0 +1,4 @@
+#ex_3_1 name.rb
+
+name = gets.chomp
+puts "Hi there, " + name + "!"
